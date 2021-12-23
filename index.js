816750df4136c8543294f2af2179fa957f0a10e9
G0J0-50 <amidoufalana00@gmail.com>
2021-12-23 17:19:47 +0000

    committing app to heroku

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { request, response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
-
+const cors = require('cors')
 
 
 const app = express()
 app.use(express.json())
-
+app.use(cors())
 
 let persons = [
   { 
