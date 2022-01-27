619754bce4d73ef020c9a5f7adb2ba23d5a0abee
G0J0-50 <amidoufalana00@gmail.com>
2022-01-27 03:20:38 +0000

    Exercise 3.22: Lint configuration

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 require('dotenv').config()
 const { request, response } = require('express')
 const express = require('express')
@@ -12,131 +13,132 @@ app.use(express.static('build'))
 
 
 let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-  ]
+	{
+		'id': 1,
+		'name': 'Arto Hellas',
+		'number': '040-123456'
+	},
+	{
+		'id': 2,
+		'name': 'Ada Lovelace',
+		'number': '39-44-5323523'
+	},
+	{
+		'id': 3,
+		'name': 'Dan Abramov',
+		'number': '12-43-234345'
+	},
+	{
+		'id': 4,
+		'name': 'Mary Poppendieck',
+		'number': '39-23-6423122'
+	}
+]
 
-  app.use(morgan((tokens, req, res) => {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      JSON.stringify(req.body)
-    ].join(' ')
-  }))
+app.use(morgan((tokens, req, res) => {
+	return [
+		tokens.method(req, res),
+		tokens.url(req, res),
+		tokens.status(req, res),
+		tokens.res(req, res, 'content-length'), '-',
+		tokens['response-time'](req, res), 'ms',
+		JSON.stringify(req.body)
+	].join(' ')
+}))
 
- 
 
-  app.get('/api/persons', (request, response) =>{
-    Person.find({}).then(person =>{
-      response.json(person)
-    })
-  })
 
-  app.get('/info', (request, response)=> {
-    const size = persons.length
-    const time = new Date()
+app.get('/api/persons', (request, response) => {
+	Person.find({}).then(person => {
+		response.json(person)
+	})
+})
 
-    response.send(`<p>Phonebook has info for ${size} people</p>
+app.get('/info', (request, response) => {
+	const size = persons.length
+	const time = new Date()
+
+	response.send(`<p>Phonebook has info for ${size} people</p>
     <p>${time}</p>
     `)
-  })
+})
 
-  app.get('/api/persons/:id', (request, response, next)=>{
+app.get('/api/persons/:id', (request, response, next) => {
 
-    Person.findById(request.params.id)
-    .then(person => {
-      if (person){
-        response.json(person)
-      }else{
-        response.status(404).end()
-      }      
-    })
-    .catch(error=> next(error))
-  })
+	Person.findById(request.params.id)
+		.then(person => {
+			if (person){
+				response.json(person)
+			}else{
+				response.status(404).end()
+			}
+		})
+		.catch(error => next(error))
+})
 
-  app.delete('/api/persons/:id', (request, response, next)=>{
-    Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error =>
-      next(error))
-  })
+app.delete('/api/persons/:id', (request, response, next) => {
+	Person.findByIdAndRemove(request.params.id)
+		.then(result => {
+			response.status(204).end()
+		})
+		.catch(error =>
+			next(error))
+})
 
-  app.post('/api/persons', (request, response) => {
-    const p = request.body
-     console.log(p);
+app.post('/api/persons', (request, response) => {
+	const p = request.body
+	console.log(p)
 
-    const person = new Person ({
-        name: p.name,
-        number: p.number,
-      }
-    )
-    const currentName = persons.filter(per => per.name === person.name)
-    
-    if(!p.name || !p.number){
-      return response.status(400).json({
-        error: `a name or number is missing`
-      })
-    }  
-    
-    if(currentName[0] !== undefined) {
-      return response.status(400).json({
-        error: `name must be unique`
-      })
-    }
-    person.save().then(addedPerson => {
-      response.json(addedPerson)
-    })
-  })
-  app.put('/api/persons/:id', (request, response, next) =>{
-    const p = request.body
-  Person.findById(request.params.id)
+	const person = new Person ({
+		name: p.name,
+		number: p.number,
+	}
+	)
+	const currentName = persons.filter(per => per.name === person.name)
 
-    const person = {
-      name: p.name,
-      number: p.number
-    }
+	if(!p.name || !p.number){
+		return response.status(400).json({
+			error: 'a name or number is missing'
+		})
+	}
 
-    Person.findByIdAndUpdate(request.params.id, person, {new: true }).then(updatedPerson => {
-      response.json(updatedPerson)
-    })
-    .catch(error => next(error))
-  })
+	if(currentName[0] !== undefined) {
+		return response.status(400).json({
+			error: 'name must be unique'
+		})
+	}
+	person.save().then(addedPerson => {
+		response.json(addedPerson)
+	})
+})
+app.put('/api/persons/:id', (request, response, next) => {
+	const p = request.body
+	Person.findById(request.params.id)
 
-const PORT = process.env.PORT 
+	const person = {
+		name: p.name,
+		number: p.number
+	}
+
+	Person.findByIdAndUpdate(request.params.id, person, { new: true }).then(updatedPerson => {
+		response.json(updatedPerson)
+	})
+		.catch(error => next(error))
+})
+
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+	console.error(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } 
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	}
 
-  next(error)
+	next(error)
 }
 app.use(errorHandler)
--- mongo.js
+++ mongo.js
@@ -1,12 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+	console.log('Please provide the password as an argument: node mongo.js <password>')
+	process.exit(1)
 }
 
 const password = process.argv[2]
-console.log(password);
+console.log(password)
 
 const url =
   `mongodb+srv://amidou:${password}@cluster0.2c3um.mongodb.net/persons?retryWrites=true&w=majority`
@@ -15,28 +17,28 @@ mongoose.connect(url)
 
 //Schema
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: Number
+	name: String,
+	number: Number
 })
 //model
 const Person = mongoose.model('Person', personSchema)
 
 //Saving data to mongo
 const person = new Person({
-  name: 'Amidou' ,
-  number: 3873209
+	name: 'Amidou' ,
+	number: 3873209
 })
 
 person.save().then(result => {
-  console.log('contact saved!')
+	console.log('contact saved!')
 })
 Person.find({})
-  .then(
-      result => {
-        result.forEach(person => {
-            console.log(person);
-        })
-        mongoose.connection.close()
-    })
-  
+	.then(
+		result => {
+			result.forEach(person => {
+				console.log(person)
+			})
+			mongoose.connection.close()
+		})
+
 
